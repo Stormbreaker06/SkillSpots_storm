ef8dd32bf7e17e5bb4e7e2901ce1dd6fe8190ba6
Masaru124 <masarukaze041@gmail.com>
2025-07-16 16:54:17 +0530

    mock data1

--- skillspots-frontend/src/pages/WorkshopDetails.jsx
+++ skillspots-frontend/src/pages/WorkshopDetails.jsx
@@ -11,7 +11,7 @@ export default function WorkshopDetails() {
 
   async function fetchWorkshop() {
     try {
-      const res = await axios.get(`http://localhost:8000/workshops/${id}`);
+      const res = await axios.get(`http://localhost:8000/workshops/${id}/`);
       setWorkshop(res.data);
     } catch (error) {
       alert("Failed to fetch workshop");
